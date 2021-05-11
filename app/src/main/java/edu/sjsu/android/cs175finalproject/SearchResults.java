31513d9c3e42cdb15b98fef584ffd7b6663dfd01
dill-larson <dillonlouislarson@gmail.com>
2021-05-11 10:50:37 -0700

    Fixed home button on search results

--- app/src/main/java/edu/sjsu/android/cs175finalproject/SearchResults.java
+++ app/src/main/java/edu/sjsu/android/cs175finalproject/SearchResults.java
@@ -2,6 +2,7 @@ package edu.sjsu.android.cs175finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +20,7 @@ public class SearchResults extends AppCompatActivity {
         avgView.setText(Double.toString(avg) + "ms");
 
     }
-    public void onClickHome(){
+    public void onClickHome(View view){
         Intent intent = new Intent(getBaseContext(), MainActivity.class);
         startActivity(intent);
     }
